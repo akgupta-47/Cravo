f12a23101b1dbf09a1cc103ab5bb4d445d55ffa3
Akshat Gupta <akshatgupta3214@gmail.com>
2025-05-28 19:52:03 +0530

    update requirements.txt

--- fastapi/start.sh
+++ fastapi/start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo "Starting FastApi Server"
+if [ "$1" == "freeze" ]; then
+    echo "Freezing requirements.txt..."
+    pip freeze > requirements.txt
+    echo "requirements.txt generated."
+else
+    echo "Starting FastApi Server"
+    fastapi dev main.py
+fi
 
-fastapi dev main.py
